4cc2ef5a64570541950550a04dd758a75f8ded21
Grindelwald <137000133@qq.com>
2021-11-01 08:16:28 +0800

    Create ArenaSpectatorNPC_SC.cpp

--- src/ArenaSpectatorNPC_SC.cpp
+++ src/ArenaSpectatorNPC_SC.cpp
@@ -59,26 +59,25 @@ public:
 
         switch (action)
         {
-        case NPC_SPECTATOR_ACTION_2V2_GAMES:
+         case NPC_SPECTATOR_ACTION_2V2_GAMES:
             AddGossipItemFor(player, 11, "< Main Menu >", GOSSIP_SENDER_MAIN, 100);
             AddGossipItemFor(player, 4, "Refresh", GOSSIP_SENDER_MAIN, NPC_SPECTATOR_ACTION_2V2_GAMES);
             sArenaSpectatorNPC->ShowPage(player, action - NPC_SPECTATOR_ACTION_2V2_GAMES, ARENA_TYPE_2v2);
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
             break;
-        case NPC_SPECTATOR_ACTION_3V3_GAMES:
+
+        }
+        if (action >= NPC_SPECTATOR_ACTION_3V3_GAMES && action < NPC_SPECTATOR_ACTION_5V5_GAMES) {
             AddGossipItemFor(player, 11, "< Main Menu >", GOSSIP_SENDER_MAIN, 100);
             AddGossipItemFor(player, 4, "Refresh", GOSSIP_SENDER_MAIN, NPC_SPECTATOR_ACTION_3V3_GAMES);
             sArenaSpectatorNPC->ShowPage(player, action - NPC_SPECTATOR_ACTION_3V3_GAMES, ARENA_TYPE_3v3);
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-            break;
-        case NPC_SPECTATOR_ACTION_5V5_GAMES:
+        } else if (action >= NPC_SPECTATOR_ACTION_5V5_GAMES && action < NPC_SPECTATOR_ACTION_SELECTED_PLAYER) {
             AddGossipItemFor(player, 11, "< Main Menu >", GOSSIP_SENDER_MAIN, 100);
             AddGossipItemFor(player, 4, "Refresh", GOSSIP_SENDER_MAIN, NPC_SPECTATOR_ACTION_5V5_GAMES);
             sArenaSpectatorNPC->ShowPage(player, action - NPC_SPECTATOR_ACTION_5V5_GAMES, ARENA_TYPE_5v5);
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
-            break;
-        }
-        if (action == 200) {
+        } else if (action == 200) {
             AddGossipItemFor(player, 11, "< Main Menu >", GOSSIP_SENDER_MAIN, 100);
             SendGossipMenuFor(player, 190017, creature->GetGUID());
         } else {
