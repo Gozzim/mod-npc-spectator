4e2e2875523ce4b84ac8f3f7423874439f100074
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-04-24 21:59:12 +0200

    chore: License Headers

--- src/spectator_loader.cpp
+++ src/spectator_loader.cpp
@@ -1,3 +1,25 @@
+/*
+ *  Module for AzerothCore edited by Gozzim (https://github.com/Gozzim)
+ *  Original code:
+ *      https://github.com/Flameshot/TrinityCore/tree/Arena-Spectator
+ *      Committed by Flameshot (https://github.com/Flameshot)
+ *      Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ *      Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 void AddSC_ArenaSpectatorNPC();
 
 void Addmod_npc_spectatorScripts()
